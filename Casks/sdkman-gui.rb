497d922e608906f07f13b95bb0c5314b4503d83b
HuangXiao <youngledo@qq.com>
2025-11-18 11:08:01 +0800

    Updated desc.

--- Casks/sdkman-gui.rb
+++ Casks/sdkman-gui.rb
@@ -2,13 +2,13 @@ cask "sdkman-gui" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.0.0"
-  sha256 arm:   "ARM64_SHA256_PLACEHOLDER",
-         intel: "X86_64_SHA256_PLACEHOLDER"
+  sha256 arm:   "7b4a80724e42fc34a4cdd22a76f899f9ca522f8dd7c5416c7f48d4c0597e5223",
+         intel: "0a7162e705d00266b061ea649285b7692eeaf3ff78b862223968c9acb7153250"
 
   url "https://github.com/youngledo/sdkman-gui/releases/download/v#{version}/sdkman-gui_#{version}_#{arch}.dmg",
       verified: "github.com/youngledo/sdkman-gui"
   name "SDKMAN GUI"
-  desc "Modern graphical management tool for SDKMAN"
+  desc "Cross-platform, supports Windows, macOS, Ubuntu. Built with JavaFX + Maven, inspired by Applite's design aesthetic, offering an elegant GUI interface for SDKMAN."
   homepage "https://github.com/youngledo/sdkman-gui"
 
   livecheck do
